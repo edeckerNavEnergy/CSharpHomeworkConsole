9ac618198a0d9e5539c362c03771fcd4ac93c972
Deckere <Deckere@L0053>
2024-07-30 06:48:11 -0600

    Add project files.

---
+++ CSharpHomeworkConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpHomeworkConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment1();
+        }
+        static void Assignment1()
+        {
+            string name;
+            //Declare other variables
+            Console.WriteLine("Enter you name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
