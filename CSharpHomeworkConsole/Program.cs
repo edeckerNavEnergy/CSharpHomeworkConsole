78c451aacc16606c6019f52de891d7d963c83590
vargasa <angelo.vargas@navenergy.com>
2024-07-30 09:12:52 -0600

    assignment #1

--- CSharpHomeworkConsole/Program.cs
+++ CSharpHomeworkConsole/Program.cs
@@ -10,11 +10,30 @@
         static void Assignment1()
         {
             string name;
+            int age;
+            double height;
+            bool student;
+
             //Declare other variables
             Console.WriteLine("Enter you name");
             name = Console.ReadLine();
 
             Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("How old are you?");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("You are an adult");
+
+            Console.WriteLine("What is your size?");
+            height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("You are really tall");
+
+            Console.WriteLine("Are you a student? true or false");
+            student = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Welcome " + name);
         }
     }
 }
