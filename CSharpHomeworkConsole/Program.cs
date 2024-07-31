66b3ce6cc5ed976fa4f45c7ffd6fcb1a3694808c
vargasa <angelo.vargas@navenergy.com>
2024-07-31 12:59:03 -0600

    assign 3

--- CSharpHomeworkConsole/Program.cs
+++ CSharpHomeworkConsole/Program.cs
@@ -41,28 +41,77 @@
             //Assignment2();
             Console.WriteLine("Thanks for using the calculator " + name);
         }
-            static void Assignment2()
+       static void Assignment2()
+        {
+            double number1, number2;
+
+
+            Console.WriteLine("Enter the first number:");
+            number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            number2 = double.Parse(Console.ReadLine());
+
+            double addition = number1 + number2;
+            double substraction = number1 - number2;
+            double multiplication = number1 * number2;
+            double division = number1 / number2;
+
+            // Mostrar resultados
+            Console.WriteLine("Addition: " + addition);
+            Console.WriteLine("Subtraction: " + substraction);
+            Console.WriteLine("Multiplication: " + multiplication);
+            Console.WriteLine("Division: " + division);
+
+            Console.ReadLine();
+
+            Assignment3();
+
+        }
+        static void Assignment3()
+        { 
+            Console.WriteLine("Ingrese su calificación (0-100):");
+        int calificacion;
+
+        
+        if (int.TryParse(Console.ReadLine(), out calificacion))
+        {
+            
+            if (calificacion >= 0 && calificacion <= 100)
             {
-                double number1, number2;
-
-              
-                Console.WriteLine("Enter the first number:");
-                number1 = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter the second number:");
-                number2 = double.Parse(Console.ReadLine());
-
-                double addition = number1 + number2;
-                double substraction = number1 - number2;
-                double multiplication = number1 * number2;
-                double division = number1 / number2;
-
-                // Mostrar resultados
-                Console.WriteLine("Addition: " + addition);
-                Console.WriteLine("Subtraction: " + substraction);
-                Console.WriteLine("Multiplication: " + multiplication);
-                Console.WriteLine("Division: " + division);
-
+                
+                if (calificacion >= 90)
+                {
+                    Console.WriteLine("Su calificacion es: A");
+                }
+                else if (calificacion >= 80)
+                {
+                    Console.WriteLine("Su calificacion es: B");
+                }
+                else if (calificacion >= 70)
+                {
+                    Console.WriteLine("Su calificacion es: C");
+                }
+                else if (calificacion >= 60)
+                {
+                    Console.WriteLine("Su calificacion es: D");
+                }
+                else
+                {
+                    Console.WriteLine("Su calificacion es: F");
+                }
             }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese una calificación dentro del rango de 0 a 100.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, ingrese un número valido.");
+        }
+            Console.ReadLine();
+
+        }
     }
 }
